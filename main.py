794e40a2547fd7eed766b9c4f548a8182f041dde
saaasssska <95924615+saaasssska@users.noreply.github.com>
2022-01-18 11:58:39 +0300

    game

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pygame
 from random import randint
+import time
 
 
 class Game():
     def __init__(self):
         self.fps_controller = pygame.time.Clock()
+        pygame.font.init()
         self.score = 0
         self.play_surface = pygame.display.set_mode((720, 460))
 
     def set_title(self):
         self.play_surface = pygame.display.set_mode((720, 460))
+
+    def show_score(self, choice=1):
+        s_font = pygame.font.SysFont('consolas', 24)
+        s_surf = s_font.render(
+            'Score ' + str(self.score), True, pygame.Color('black'))
+        s_rect = s_surf.get_rect()
+        if choice == 1:
+            s_rect.midtop = (80, 10)
+        else:
+            s_rect.midtop = (360, 120)
+        self.play_surface.blit(s_surf, s_rect)
 
     def events(self, change_to):
         for event in pygame.event.get():
@@ -99,25 +112,60 @@ class Food():
                 self.food_size_x, self.food_size_y))
 
 
-game = Game()
-snake = Snake()
-food = Food()
-game.set_title()
-screen_width = 720
-screen_height = 460
-pygame.display.set_caption('Snake Game')
+def main():
+    game = Game()
+    snake = Snake()
+    food = Food()
+    game.set_title()
+    screen_width = 720
+    screen_height = 460
+    pygame.display.set_caption('Snake Game')
 
-while True:
-    snake.change_to = game.events(snake.change_to)
-    snake.my_direction()
-    snake.where_head()
-    game.score, food.food_pos = snake.body_mechanism(game.score, food.food_pos, 720, 460)
-    snake.draw_snake(game.play_surface, (255, 255, 255))
+    while True:
+        snake.change_to = game.events(snake.change_to)
+        snake.my_direction()
+        snake.where_head()
+        game.score, food.food_pos = snake.body_mechanism(game.score, food.food_pos, 720, 460)
+        snake.draw_snake(game.play_surface, (255, 255, 255))
 
-    food.draw_food(game.play_surface)
+        food.draw_food(game.play_surface)
 
-    snake.check_for_boundaries(
-        game.game_over, 720, 460)
+        snake.check_for_boundaries(
+            game.game_over, 720, 460)
+        game.show_score()
+        pygame.display.flip()
+        game.fps_controller.tick(10)
 
-    pygame.display.flip()
-    game.fps_controller.tick(10)
+
+def start_screen():
+    intro_text = ["                 Змейка",
+                  "Кликните тобы начать игру",]
+    screen = pygame.display.set_mode((720, 460))
+    pygame.font.init()
+    clock = pygame.time.Clock()
+
+    fon = pygame.transform.scale(pygame.image.load('textures\img.png'), (720, 460))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 60)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, True, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 100
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+        pygame.display.flip()
+        clock.tick(60)
+
+start_screen()
